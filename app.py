a3f393608fb5cac253e97cbcb741be4a29266d40
Izaias Lima <izaiaslima356@gmail.com>
2024-03-20 12:28:06 -0300

    feat: create get product by id, update product and get products routes

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_login import UserMixin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
+CORS(app)
+
+class User(db.Model, UserMixin):
+		id = db.Column(db.Integer, primary_key=True)
+		username = db.Column(db.String(80), unique=True, nullable=False)
 
 class Product(db.Model):
 		id = db.Column(db.Integer, primary_key=True)
@@ -30,6 +37,46 @@ def delete_product(product_id):
 		db.session.commit()
 		return jsonify({'message': 'Product deleted successfully!'})
 	return jsonify({'message': 'Product not found!'}), 404
+
+@app.route('/api/products/<int:product_id>', methods=['GET'])
+def get_product_details(product_id):
+	product = Product.query.get(product_id)
+	if product:
+		return jsonify({
+			'id': product.id,
+			'name': product.name,
+			'price': product.price,
+			'description': product.description
+		})
+	return jsonify({'message': 'Product not found!'}), 404
+
+@app.route('/api/products/update/<int:product_id>', methods=['PUT'])
+def update_product(product_id):
+	product = Product.query.get(product_id)
+	if product:
+		data = request.json
+		if 'name' in data:
+			product.name = data['name']
+		if 'price' in data:
+			product.price = data['price']
+		if 'description' in data:
+			product.description = data['description']
+		db.session.commit()
+		return jsonify({'message': 'Product updated successfully!'})
+	return jsonify({'message': 'Product not found!'}), 404
+
+@app.route('/api/products', methods=['GET'])
+def get_products():
+	products = Product.query.all()
+	response = []
+	for product in products:
+		response.append({
+			'id': product.id,
+			'name': product.name,
+			'price': product.price,
+			'description': product.description
+		})
+	return jsonify(response)
 
 @app.route('/')
 def hello_world():
