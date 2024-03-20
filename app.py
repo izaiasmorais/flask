4f9590b7e5d4ef0cd6e487a95a9dd27328188cee
Izaias Lima <izaiaslima356@gmail.com>
2024-03-19 22:14:23 -0300

    feat: set up sqlite database

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+		id = db.Column(db.Integer, primary_key=True)
+		name = db.Column(db.String(120), nullable=False)
+		price = db.Column(db.Float, nullable=False)
+		descrotion = db.Column(db.Text, nullable=True)
 
 @app.route('/')
 def hello_world():
